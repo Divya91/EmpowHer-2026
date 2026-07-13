743063a6f0a09bd3ee6945c419c344d2f2b9e8d0
Moulica15 <moulicachowdary@gmail.com>
2026-07-13 16:04:59 +0530

    Complete Flight Management Module with CRUD, DTO Mapping, Validation, Dynamic Search and JPA Specifications

---
+++ Moulica/src/main/java/com/flight/booking/entity/Role.java
@@ -0,0 +1,9 @@
+package com.flight.booking.entity;
+
+public enum Role {
+
+    USER,
+
+    ADMIN
+
+}
---
+++ Moulica/src/main/java/com/flight/booking/entity/User.java
@@ -0,0 +1,37 @@
+package com.flight.booking.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users", schema = "flight_booking_system")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
